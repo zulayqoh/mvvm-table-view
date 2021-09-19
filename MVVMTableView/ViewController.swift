c579650e5c39c2a9a01d812882b57072bd022778
Temitope <temitope.babatunde@decagon.dev>
2021-09-19 20:53:09 +0100

    make API call

--- MVVMTableView/ViewController.swift
+++ MVVMTableView/ViewController.swift
@@ -8,10 +8,16 @@
 import UIKit
 
 class TableViewController: UITableViewController {
+  
+  var users : [User] = [] {
+    didSet {
+      tableView.reloadData()
+    }
+  }
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    fetchUsers()
   }
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
@@ -19,6 +25,16 @@ class TableViewController: UITableViewController {
     cell.detailTextLabel?.text = "details"
     
     return cell
+  }
+  
+  private func fetchUsers() {
+    APImanager.shared.fetchUsers { (result) in
+      switch result {
+      case .success(let users): self.users = users
+      case .failure(let error): print(error.localizedDescription)
+      }
+    }
+    
   }
   
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -29,5 +45,45 @@ class TableViewController: UITableViewController {
     2
   }
 
+}
+
+class APImanager {
+  
+  static let shared = APImanager()
+  
+  private init() {
+    
+  }
+  
+  func fetchUsers(completion: @escaping (Result<[User], Error>) -> Void) {
+    
+    let urlString = "https://jsonplaceholder.typicode.com/users"
+    guard let url = URL(string: urlString) else {return}
+    URLSession.shared.dataTask(with: url) { data, response, error in
+      if let error = error {
+        completion(.failure(error))
+        return
+      }
+      guard let data = data else {
+        fatalError("Data cannot be found")
+      }
+      
+      do {
+        let users = try JSONDecoder().decode([User].self, from: data)
+      }
+      catch(let error) {
+        completion(.failure(error))
+      }
+    }.resume()
+    
+  }
+
+  
+}
+
+struct User: Decodable {
+  let id: Int
+  let name: String
+  let email: String
 }
 
