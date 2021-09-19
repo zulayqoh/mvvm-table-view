5a2e40a61b043524b024225132f7ba8e24cb9f95
Temitope <temitope.babatunde@decagon.dev>
2021-09-19 21:44:19 +0100

    MVC version

--- MVVMTableView/ViewController.swift
+++ MVVMTableView/ViewController.swift
@@ -19,32 +19,35 @@ class TableViewController: UITableViewController {
     super.viewDidLoad()
     fetchUsers()
   }
-  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
-    cell.textLabel?.text = "SubTitle"
-    cell.detailTextLabel?.text = "details"
-    
-    return cell
-  }
   
   private func fetchUsers() {
+    
     APImanager.shared.fetchUsers { (result) in
       switch result {
-      case .success(let users): self.users = users
+      case .success(let users):
+        DispatchQueue.main.async {
+          self.users = users
+        }
       case .failure(let error): print(error.localizedDescription)
       }
     }
-    
   }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
+    let user = users[indexPath.item]
+    
+    cell.textLabel?.text = user.name
+    cell.detailTextLabel?.text = user.email
+    return cell
+  }
+  
+  
   
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    5
+    users.count
   }
-  
-  override func numberOfSections(in tableView: UITableView) -> Int {
-    2
-  }
-
 }
 
 class APImanager {
@@ -52,33 +55,35 @@ class APImanager {
   static let shared = APImanager()
   
   private init() {
-    
   }
   
   func fetchUsers(completion: @escaping (Result<[User], Error>) -> Void) {
     
     let urlString = "https://jsonplaceholder.typicode.com/users"
     guard let url = URL(string: urlString) else {return}
+    
     URLSession.shared.dataTask(with: url) { data, response, error in
+      
       if let error = error {
         completion(.failure(error))
         return
       }
+      
       guard let data = data else {
         fatalError("Data cannot be found")
       }
       
       do {
         let users = try JSONDecoder().decode([User].self, from: data)
+        completion(.success(users))
       }
+      
       catch(let error) {
         completion(.failure(error))
       }
+      
     }.resume()
-    
   }
-
-  
 }
 
 struct User: Decodable {
